e92da87de1d3b1f60d8a71e95098d916b9ac0e59
Aaron Steven White <aswhite@jhu.edu>
2018-09-08 14:03:22 -0400

    updated handling of submission

--- js/webspr/main.js
+++ js/webspr/main.js
@@ -2,7 +2,7 @@ var serverURI = "server.py";
 
 var body = document.getElementsByTagName("body")[0];
 
-var submit = document.getElementById('submitButton');
+var submit = document.getElementById('submit');
 submit.hide();
 
 counter = Math.floor(Math.random() * 10000);
